ac895a3a6ed1b6314b2f23e45f86312acbbfbb49
jamesb <james.a.bristow.contact@gmail.com>
2017-04-07 19:02:56 +1200

    Added assets

--- src/Breakout.ts
+++ src/Breakout.ts
@@ -1,7 +1,41 @@
-export class Breakout extends Phaser.Game {
-    constructor() {
+export class Breakout extends Phaser.Game
+{
+
+    /*=============================
+    **Fields**
+    =============================*/
+    _gameName: string; 
+    _gameDescription: string;
+    _playerName: string;
+
+    /*=============================
+    **Constructors
+    =============================*/
+
+    constructor()
+    {
         super();
-        
-        document.writeln("Hello, World! from Phaser-" + Phaser.VERSION);
+        this._gameName = "Breakout Turbo";
+        this._gameDescription = "Hit the ball with the paddle to ensure that the ball does not fall below the bottom edge of the screen." + "<br>" +
+            "Shattered bricks will drop coins. Collect coins to gain points." + "<br>" +
+            "Enemies can attack your ball and paddle. Watch out!";
+        this.setOptions();
+    }
+
+    /*=============================
+    **Properties**
+    =============================*/
+
+    /*=============================
+    **Methods**
+    =============================*/
+
+    setOptions()//Options are alterable at run-time
+    {
+
+    }
+
+    preload() {
+
     }
 }
